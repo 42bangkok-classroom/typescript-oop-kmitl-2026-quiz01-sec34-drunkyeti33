bfeb81abf059a73b1e2663c9450e54e5ba1192d9
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2025-12-15 17:54:03 +0700

    a

--- src/q16/ValidatePass.ts
+++ src/q16/ValidatePass.ts
@@ -1,5 +1,8 @@
 const password = (process.argv[2])
-if (password.length >= 8)
+function hasAtLeastOneUppercase(input: string): boolean {
+  return /[A-Z]/.test(input);
+}
+if (password.length >= 8 && hasAtLeastOneUppercase &&  (password.includes("1") || password.includes("2") || password.includes("3") || password.includes("4") || password.includes("5") || password.includes("6") || password.includes("7") || password.includes("8") || password.includes("9") || password.includes("0")))
 {
   console.log("Valid")
 }
